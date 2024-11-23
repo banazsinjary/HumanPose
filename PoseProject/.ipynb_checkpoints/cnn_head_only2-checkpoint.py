3057bade8eaa0514eab1b8f765523930881eaff5
banaz <banazsinjary@gmail.com>
2024-11-23 11:55:33 -0800

    MediaPipe Set Up

---
+++ PoseProject/.ipynb_checkpoints/cnn_head_only2-checkpoint.py
@@ -0,0 +1,244 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 31 13:54:06 2024
+
+@author: Charl
+"""
+
+import pandas as pd
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import os
+from PIL import Image
+from torchvision import transforms
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
+from sklearn.model_selection import train_test_split
+
+# set project and image directories
+proj_dir = r"C:\Users\Charl\Documents\College\2 - CSC 871\project\mpii_human_pose\\"
+img_dir = proj_dir + "images"
+
+# load label data
+image_labels = pd.read_csv(proj_dir + "labels.csv", index_col=0)
+
+
+class HeadDataset(Dataset):
+    def __init__(self, images, loc_and_scale, head_positions):
+        self.images = images
+        self.loc_and_scale = loc_and_scale
+        self.head_positions = head_positions  # Head (x, y) positions
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        image = self.images[idx]
+        loc_and_scale = self.loc_and_scale[idx]
+        head_position = self.head_positions[idx]  # (head_x, head_y)
+        return image, loc_and_scale, head_position
+
+
+# Define transformations
+H = 360; W = 640
+transform = transforms.Compose([
+    transforms.Resize((H, W)),  # Resize to target size
+    transforms.ToTensor(),  # Convert to tensor and scale to [0, 1]
+    #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize
+])
+
+class FeatureExtraction(nn.Module):
+    def __init__(self, input_channels):
+        super(FeatureExtraction, self).__init__()
+        
+        # input_shape: height, width, and # of channels of images
+        # num_joints: number of joints to be iden
+
+        # Shared Convolutional Layers
+        self.conv1 = nn.Conv2d(input_channels, 32, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))  # Global average pooling
+    
+    
+    def forward(self, x):
+        # Shared Convolutional Layers
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        
+        # Global Average Pooling to reduce spatial dimensions
+        x = self.global_avg_pool(x)
+        #x = torch.flatten(x, 1)  # Flatten to (batch_size, 128)
+        x = x.view(x.size(0), -1)
+
+        return x
+    
+# CNN model for head position detection
+class HeadDetectionCNN(nn.Module):
+    def __init__(self, input_channels=3):
+        super(HeadDetectionCNN, self).__init__()
+        
+        self.cnn = FeatureExtraction(input_channels)
+        
+        self.fc1 = nn.Linear(128 + 3, 64)
+        self.fc2 = nn.Linear(64,32)
+        
+        self.joint_coordinates = nn.Linear(32, 2)
+        
+        #self.fc_input_size = H*W
+
+    def forward(self, image, loc_and_scale):
+        
+        image_features = self.cnn(image)
+        
+        combined_features = torch.cat((image_features, loc_and_scale), dim=1)
+        
+        # Pass through fully connected layers
+        x = F.relu(self.fc1(combined_features))
+        x = F.relu(self.fc2(x))
+        
+        # Separate outputs for coordinates and visibility
+        joint_coords = self.joint_coordinates(x)
+        
+        return joint_coords
+
+# Load images
+def load_images(folder_path, file_list):
+    images = []
+    incl_files = []
+    for filename in file_list: 
+        if filename.endswith(".jpg"):
+            img_path = os.path.join(folder_path, filename)
+            try:
+                image = Image.open(img_path).convert('RGB') 
+            except FileNotFoundError:
+                print(filename)
+                continue
+            image = transform(image)  
+            images.append(image)
+            incl_files = incl_files + [filename]
+    return torch.stack(images), incl_files  
+
+
+# create dataset from labels, images and filenames
+def getDataset(labels, images, files):
+    label_set = labels[labels['img_name'].isin(files)] 
+    loc_and_scale = torch.tensor(label_set[['objpos_x', 'objpos_y', 'scale']].values, dtype=torch.float32)
+    head_loc = torch.tensor(label_set[['head_x', 'head_y']].values, dtype=torch.float32)
+
+    dataset = HeadDataset(images=images, 
+                          loc_and_scale=loc_and_scale, 
+                          head_positions=head_loc)
+    return(dataset)
+
+### Load data and create DataLoaders
+
+##### TODO -->
+# load data in parts to conserve memory
+
+# get list of filenames
+img_filenames = image_labels['img_name'].values
+
+# load images (780 sec)
+start = time.time()
+image_data, incl_files = load_images(img_dir, img_filenames) # too much, need to do in parts
+end = time.time()
+end - start
+
+# split train/validation/test (360 sec)
+start = time.time()
+image_train, image_test, files_train, files_test = train_test_split(
+    image_data, incl_files, test_size=0.2, random_state=1)
+image_train, image_val, files_train, files_val = train_test_split(
+    image_train, files_train, test_size=0.25, random_state=1)
+end = time.time()
+end - start
+
+
+# load training data
+train_dataset = getDataset(image_labels, image_train, files_train)
+train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+
+# load validation data
+val_dataset = getDataset(image_labels, image_val, files_val)
+val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=True)
+
+# load test data
+test_dataset = getDataset(image_labels, image_test, files_test)
+test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+###
+
+# Initialize model, loss, and optimizer
+model = HeadDetectionCNN()
+criterion = nn.MSELoss()  # Mean Squared Error Loss for coordinate prediction
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Training loop (5 epochs -> 8796 sec)
+epochs = 5
+start = time.time()
+for epoch in range(epochs):
+    
+    epoch_start = time.time()
+    model.train()
+    running_loss = 0.0
+    running_val_loss = 0.0
+        
+    for images, locs_and_scales, head_positions in train_dataloader:
+        optimizer.zero_grad()
+        
+        # Forward pass
+        outputs = model(images, locs_and_scales)
+        loss = criterion(outputs, head_positions)
+        
+        # Backward pass and optimization
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+
+    train_time = time.time()
+    print(f"Epoch [{epoch+1}/{epochs}] ({train_time-epoch_start}), Training Loss: {running_loss/len(train_dataloader):.2f}")
+    
+    for images, locs_and_scales, head_positions in val_dataloader:
+        
+        model.eval()  # Set the model to evaluation mode
+        with torch.no_grad():
+            outputs = model(images, locs_and_scales)
+        
+        val_loss = criterion(outputs, head_positions)
+        
+        running_val_loss += val_loss.item()
+
+    val_time = time.time()
+    print(f"Epoch [{epoch+1}/{epochs}] ({val_time-train_time}), Validation Loss: {running_val_loss/len(val_dataloader):.2f}")
+    
+end = time.time()
+print(f"Training completed ({end - start})")
+
+
+model.eval()  # Set the model to evaluation mode
+with torch.no_grad():
+    running_test_loss = 0.0
+        
+    for images, locs_and_scales, head_positions in test_dataloader:
+        
+        joint_loc_preds = model(images, locs_and_scales)
+        test_loss = criterion(joint_loc_preds, head_positions)
+        
+        running_test_loss += test_loss.item()
+
+    print(f"Test Loss: {running_test_loss/len(test_dataloader):.2f}")
+
+
+##### TODO -->
+# create a set of test outputs and visualize
+
+
+# save model
+torch.save(model, proj_dir + 'model.pth') 
+torch.save(model.state_dict(), proj_dir + 'model_state_dict.pth')
---
+++ PoseProject/mpii_label_extraction.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 29 17:37:18 2024
+
+"""
+
+import os
+import scipy.io
+import pandas as pd
+import copy
+
+# set directory and get filenames
+proj_dir = r"C:\Users\Charl\Documents\College\2 - CSC 871\project\mpii_human_pose\\"
+filenames = os.listdir(proj_dir + "images")
+
+# index to joint name conversion
+mpii_idx_to_jnt = {0: 'rankl', 1: 'rknee', 2: 'rhip', 3: 'lhip', 4: 'lknee', 5: 'lankl', 
+                   6: 'pelvis', 7: 'thorax', 8: 'upper_neck', 9: 'head', 10: 'rwri', 
+                   11: 'relb', 12: 'rsho', 13: 'lsho', 14: 'lelb', 15: 'lwri'}
+
+# load mat file
+matlab_mpii = scipy.io.loadmat(os.path.join(proj_dir, 'mpii_human_pose_v1_u12_1.mat'), 
+                               struct_as_record=False)['RELEASE'][0, 0]
+
+#matlab_mpii.__dict__
+#matlab_mpii.__dict__['annolist'][0, 0].__dict__
+#annolist_train = matlab_mpii.__dict__['single_person'][matlab_mpii.__dict__['img_train'][0]==1]
+#annolist_test = matlab_mpii.__dict__['single_person'][matlab_mpii.__dict__['img_train'][0]==0]
+
+
+# get number of images
+num_images = matlab_mpii.__dict__['annolist'][0].shape[0]
+
+##### TODO -->
+# get category and activity
+
+# names of label columns
+label_colnames = ['img_name', 'person', 'category', 'activity',
+                  'objpos_x', 'objpos_y', 'scale',
+                  'rankl', 'rankl_x', 'rankl_y', 
+                  'rknee', 'rknee_x', 'rknee_y',
+                  'rhip', 'rhip_x', 'rhip_y',
+                  'lhip', 'lhip_x', 'lhip_y',
+                  'lknee', 'lknee_x', 'lknee_y', 
+                  'lankl', 'lankl_x', 'lankl_y',
+                  'pelvis', 'pelvis_x', 'pelvis_y',
+                  'thorax', 'thorax_x', 'thorax_y',
+                  'upper_neck', 'upper_neck_x', 'upper_neck_y',
+                  'head', 'head_x', 'head_y',
+                  'rwri', 'rwri_x', 'rwri_y',
+                  'relb', 'relb_x', 'relb_y',
+                  'rsho', 'rsho_x', 'rsho_y',
+                  'lsho', 'lsho_x', 'lsho_y',
+                  'lelb', 'lelb_x', 'lelb_y', 
+                  'lwri', 'lwri_x', 'lwri_y']
+
+# template for joint dictionary
+joint_dict_template = {}
+for i in range(16): joint_dict_template[i] = (0,0,0)
+
+# dataframes for storing labels
+training_labels = pd.DataFrame(columns=label_colnames)
+test_labels = pd.DataFrame(columns=label_colnames)
+
+# iterate through images to extract labels of images with 0 or 1 person in them
+for i in range(num_images):
+
+    # check # of annotated single persons in image
+    person_id = matlab_mpii.__dict__['single_person'][i][0].flatten()    
+    if len(person_id) > 1: continue # exclude images with more than 1 person
+        
+    # get annotations, training/test allocation, name, category and activity of image
+    annotations = matlab_mpii.__dict__['annolist'][0, i]
+    train_test_mpii = matlab_mpii.__dict__['img_train'][0, i].flatten()[0]
+    img_name = annotations.__dict__['image'][0, 0].__dict__['name'][0]
+
+    act = matlab_mpii.__dict__['act'][i, 0]    
+    img_cat = act.__dict__['cat_name'].tolist()
+    if len(img_cat) == 0:
+        img_cat = ""
+    else:
+        img_cat = img_cat[0]
+    img_act = act.__dict__['act_name'].tolist()
+    
+    ### dropping images with no annotated person(s) in them
+    # set labels of images without an annotated person in them
+    # if len(person_id) < 1:
+    #     # drop image without an annotated single person in them
+    #     img_labels = [img_name] + [0]*52
+    #     if train_test_mpii == 1:
+    #         training_labels = pd.concat([training_labels, pd.DataFrame([img_labels], columns = label_colnames)])
+    #     elif train_test_mpii == 0:
+    #         test_labels = pd.concat([test_labels, pd.DataFrame([img_labels], columns = label_colnames)])
+    #     else:
+    #         print([train_test_mpii, img_name])
+        
+    
+    ##### TODO -->
+    # keep 1st single person annotation in images with more than 1
+    
+    # set labels of images with 1 person in them
+    if len(person_id) == 1:
+        joint_dict = copy.deepcopy(joint_dict_template)
+        # use try/catch to handle key and index errors
+        try:
+            # get annotated points and number of joints in image
+            annorect_img_mpii = annotations.__dict__['annorect'][0, person_id-1][0]
+            obj_pos = annorect_img_mpii.__dict__['objpos'][0, 0]
+            scale = annorect_img_mpii.__dict__['scale'][0][0]
+            annopoints_img_mpii = annorect_img_mpii.__dict__['annopoints'][0, 0]
+            num_joints = annopoints_img_mpii.__dict__['point'][0].shape[0]
+            
+            # create base for image labels for this image
+            img_labels = [img_name, 1, img_cat, img_act, 
+                          obj_pos.__dict__['x'].flatten()[0], 
+                          obj_pos.__dict__['y'].flatten()[0], scale]
+            for j in range(num_joints):
+                # check visibility of joint
+                vis = annopoints_img_mpii.__dict__['point'][0, j].__dict__['is_visible'].flatten()
+                # head and neck have no entry for vis (it is an empty array)
+                # so only drop joints that are set as not visible
+                if vis.size > 0 and vis[0] == 0: continue
+            
+                # get id and location of joint and store in dictionary
+                x = annopoints_img_mpii.__dict__['point'][0, j].__dict__['x'].flatten()[0]
+                y = annopoints_img_mpii.__dict__['point'][0, j].__dict__['y'].flatten()[0]
+                id_ = annopoints_img_mpii.__dict__['point'][0, j].__dict__['id'][0][0]
+                joint_dict[id_] = (1, x, y)
+                
+            # add joint visibility and locations to labels for this image
+            for j_id in range(16):
+                img_labels = img_labels + [*joint_dict[j_id]]
+                
+            # add image labels to appropriate df
+            if train_test_mpii == 1:
+                training_labels = pd.concat([training_labels, pd.DataFrame([img_labels], columns = label_colnames)])
+            elif train_test_mpii == 0:
+                test_labels = pd.concat([test_labels, pd.DataFrame([img_labels], columns = label_colnames)])
+            else:
+                print([train_test_mpii, img_name])
+        
+        # catch errors
+        except KeyError:
+            # no annotated joints: drop from dataset
+            # print('Key Error: image #' + str(i) + ", " + img_name)
+            # img_labels = [img_name, 1] + [0]*51
+            # if train_test_mpii == 1:
+            #     training_labels = pd.concat([training_labels, pd.DataFrame([img_labels], columns = label_colnames)])
+            # elif train_test_mpii == 0:
+            #     test_labels = pd.concat([test_labels, pd.DataFrame([img_labels], columns = label_colnames)])
+            # else:
+            #     print([train_test_mpii, img_name])
+            continue
+        except IndexError:
+            # images with more than 1 person but only 1 is annotated
+            # print(i, img_name)
+            continue
+    
+training_labels.to_csv(proj_dir + "labels.csv")
+#test_labels.to_csv(proj_dir + "test_labels.csv")
